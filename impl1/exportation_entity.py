db0e8f7d78a4e7c15f4e5ec36b05e2aaefc5f046
William Ramos <contato.williamramos@outlook.com>
2025-06-02 20:56:52 -0300

    Remoção da versão não modularizada do projeto

--- impl1/exportation_entity.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, model_validator
-
-class ExportationEntity(BaseModel):
-
-    id: int
-    country: str
-    year: int
-    exportation: int
-    
--- impl1/pc_categories_enum.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class ProcessingCategoryEnum(Enum):
-    """
-    Enum class for representing categories.
-    """
-    # Define your categories here
-    TI = "Tintas"
-    BR = "Brancas e Rosadas"
